f53bcc43f6adb031647a9ab9535005bff39352ab
Rajatha <149949532+Rajatha19@users.noreply.github.com>
2025-07-28 23:10:57 +0530

    Update exportCSV.ts

--- src/utils/exportCSV.ts
+++ src/utils/exportCSV.ts
@@ -1,7 +1,13 @@
 import Papa from "papaparse";
 import { saveAs } from "file-saver";
 
-export function exportAsCSV(data: any[], filename = "dashboard-data.csv") {
+type DataRow = {
+  user: string;
+  email: string;
+  status: string;
+};
+
+export function exportAsCSV(data: DataRow[], filename = "dashboard-data.csv") {
   const csv = Papa.unparse(data);
   const blob = new Blob([csv], { type: "text/csv" });
   saveAs(blob, filename);
